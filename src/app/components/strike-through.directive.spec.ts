c3cf6a8cda524d691796f720fae22928bc571dca
Kevin Munene <Nesh@Retinas-MacBook-Pro.local>
2020-05-07 09:00:00 +0300

    Auto-generated commit

--- src/app/components/strike-through.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { StrikeThroughDirective } from './strike-through.directive';
-//
-// describe('StrikeThroughDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new StrikeThroughDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
